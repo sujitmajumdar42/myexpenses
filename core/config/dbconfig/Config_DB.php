9c663c73a8d6aefcfca61ff21cd764e81a43b1b0
sujit <sujit@Cipher>
2014-05-31 12:15:34 +0530

    Modified Config_DB.php Added Constants,

--- core/config/dbconfig/Config_DB.php
+++ core/config/dbconfig/Config_DB.php
@@ -1,23 +1,35 @@
 <?php
 
-function dbConnect(){
-	$db_host="mysql5.000webhost.com";
-	$db_username="a7519541_admin";
-	$db_pass="Sujit@42";
-	$db_name="a7519541_expdb";
-	mysql_connect($db_host, $db_username,$db_pass) or die(mysql_error());
-	mysql_select_db($db_name) or die(mysql_error());
+/**
+ * Database credentials
+ */
+define('DB_HOST', 'mysql5.000webhost.com');
+define('DB_USER_NAME', 'a7519541_admin');
+define('DB_USER_PASS', 'admin@123');
+define('DB_NAME', 'a7519541_dbexp');
+
+/**
+ * <u>Database Configuration</u>
+ * This class creates a database connection and<br/>
+ * this class is used to execute SQL Query
+ */
+class dbConfig {
+    /* Default constructor
+     * It is used to load the database and create a connection pool
+     */
+
+    function __construct() {
+        mysql_connect(DB_HOST, DB_USER_NAME, DB_USER_PASS) or die(mysql_error());
+        mysql_select_db(DB_NAME);
+    }
+
+    /**
+     * executes sql statement.
+     * @param String $sql SQL statement
+     * @throws SQL Exception
+     */
+    function execute($sql) {
+        return mysql_query($sql) or die(mysql_error());
+    }
+
 }
-function dbDisconnect(){
-}
-function checkDb(){
-	dbConnect();
-	echo "Connected to MySQL<br />";
-}
-function dbChkUser($uname,$pass){
-	$query = "SELECT COUNT(*) AS USER FROM `user_login` WHERE USER_NAME='".$uname."' AND PASSWORD='".$pass."'";
-	$result = mysql_query($query) or die(mysql_error());
-	$resp =   mysql_fetch_array( $result );
-	return $resp['USER'];
-}
-?>
